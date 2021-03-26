c9c56d8316b9b292d60548b6d39990f0fdde9ec6
she-writescode <busolaokemoney@gmail.com>
2021-03-26 17:48:49 +0100

    current fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: [
+      "./public/**/*.html",
+      "./public/**/*.js",
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
